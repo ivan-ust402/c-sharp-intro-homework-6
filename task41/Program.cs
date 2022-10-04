8a06024d2a79cb10a89d9d5a975963b91d843893
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-04 20:21:16 +0700

    Created HandleFillArray and FindCountPositiveDigit functions

---
+++ task41/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+Задача 41: Пользователь вводит с клавиатуры число M, потом вводит M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+5, 0, 7, 8, -2, -2 -> 2
+
+5, 1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+int length = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[length];
+
+int[] HandleFillArray(int[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        arr[i] = int.Parse(Console.ReadLine()!);
+    }
+    return arr;
+}
+
+int FindCountPositiveDigit(int[] arr) {
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
