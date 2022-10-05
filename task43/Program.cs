1e229cf90d2f384a2745762298ed4ba5c9496672
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-05 22:37:19 +0700

    Complited task 43

--- task43/Program.cs
+++ task43/Program.cs
@@ -6,27 +6,33 @@ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.Clear();
 Console.WriteLine("----- Программа для нахождения точки пересечения двух прямых -----");
 
-Console.WriteLine("Введите коэффициент k1: ");
-int k1 = int.Parse(Console.ReadLine()!);
+
 Console.WriteLine("Введите коэффициент b1: ");
-int b1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите коэффициент k2: ");
-int k2 = int.Parse(Console.ReadLine()!);
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите коэффициент b2: ");
-int b2 = int.Parse(Console.ReadLine()!);
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
 
-int[] array = FindIntersectionPoint(k1, b1, k2, b2);
+if (k1 != 0 && b1 != 0 && k2 != 0 && b2 != 0) {
+    double[] array = FindIntersectionPoint(k1, b1, k2, b2);
+    Console.Write("(");
+    Console.Write(array[0]);
+    Console.Write(", ");
+    Console.Write(array[1]);
+    Console.Write(")");
+} else {
+    Console.WriteLine("В уравнении для прямой коэффициеты k и b должны быть действительными числами и НЕ равны НУЛЮ! Попробуйте перезапустить программу и ввести коэффициеты заново!");
+}
 
-Console.Write("(");
-Console.Write(array[0]);
-Console.Write(", ");
-Console.Write(array[1]);
-Console.Write(")");
 
-int[] FindIntersectionPoint(int k1, int b1, int k2, int b2) {
-    int x = (b2 - b1) / (k1 - k2);
-    int y = k1 * x + b1;
-    int[] array = new int[]{x, y};
-    return array;  
+
+double[] FindIntersectionPoint(double k1, double b1, double k2, double b2) {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        double[] array = new double[]{x, y};
+        return array;  
 }
 
